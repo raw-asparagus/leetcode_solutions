ffbda33efcebbd2c5d6282044f18fe9ed97c19ff
not jr <66353964+raw-asparagus@users.noreply.github.com>
2024-04-04 19:45:14 +0800

    Time: 8 ms (82.19%) | Memory: 6.5 MB (99.29%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.c
+++ 42-trapping-rain-water/trapping-rain-water.c
@@ -6,27 +6,27 @@ int trap(int* height, int heightSize) {
     int j = heightSize - 1;
     int right_max = height[j];  // Right
 
-    // Iterates until right meets left
+    //  Iterates as long as left is left of right
     while (i < j) {
         //  Right is taller
         if (height[i] < height[j]) {
             //  Next left is shorter than previous
             if (height[i] < left_max)
                 acc += left_max - height[i];
-            //  Next left is taller than previous
-            else
-                left_max = height[i];
+            //  Next left is taller than or same
+            //  as previous
+            else left_max = height[i];
             
             i++;
         }
         //  Left is taller or both are same height
         else {
-            // Previous right is shorter than next
+            //  Previous right is shorter than next
             if (height[j] < right_max)
                 acc += right_max - height[j];
-            // Previous right is taller than next
-            else
-                right_max = height[j];
+            //  Previous right is taller than or
+            //  same as next
+            else right_max = height[j];
             
             j--;
         }
