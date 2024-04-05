5b81020230edc98575336c0895d54bf1405b400d
not jr <66353964+raw-asparagus@users.noreply.github.com>
2024-04-05 21:42:52 +0800

    Time: 44 ms (77.76%) | Memory: 16.6 MB (20.71%) - LeetSync

--- 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.py
+++ 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.py
@@ -1,14 +1,12 @@
 class Solution:
+    #   Iterate through nums, returns iterated
+    #   num and nested iterated num pair if both
+    #   num fulfill conditions
     def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
-        #   Iterate through nums for first operand, then iterate through nums for second operand
-        #   excluding iterated first operands
-        nums_len = len(nums)
-
-        for i in range(nums_len):
-            #   Start at index at least 'indexDifference' away
-            for j in range(i + indexDifference, nums_len):
-                if abs(nums[i] - nums[j]) >= valueDifference:
-                    return [i, j]
+        for i_idx, i_val in enumerate(nums):
+            for j_idx, j_val in enumerate(nums[i_idx + indexDifference:], start = i_idx + indexDifference):
+                if abs(i_val - j_val) >= valueDifference:
+                    return [i_idx, j_idx]
         
         #   Default
         return  [-1, -1]
