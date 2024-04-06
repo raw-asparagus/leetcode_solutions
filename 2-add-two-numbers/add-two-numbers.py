9b3f4721ce20f4e1c1735759f7575dad2fde7031
not jr <66353964+raw-asparagus@users.noreply.github.com>
2024-04-06 13:31:26 +0800

    Time: 50 ms (83.22%) | Memory: 16.8 MB (9.78%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -9,7 +9,7 @@ class Solution:
     #   carry out. Save sum as a new linked list
     #   node
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        res = return_res = ListNode()
+        res = head = ListNode()
         carry = 0
 
         while l1 or l2 or carry:
@@ -18,9 +18,11 @@ class Solution:
             l2_val = l2.val if l2 else 0
             l2 = l2.next if l2 else None
 
-            carry, rem = divmod(l1_val + l2_val + carry, 10)
+            add = l1_val + l2_val + carry
+            carry = add // 10
+            rem = add % 10
 
             res.next = ListNode(rem)
             res = res.next
 
-        return return_res.next
+        return head.next
