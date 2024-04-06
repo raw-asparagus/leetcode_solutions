a94e97a40ebab3077d235850a043b7d27727cf24
not jr <66353964+raw-asparagus@users.noreply.github.com>
2024-04-06 09:42:00 +0800

    Time: 2932 ms (86.11%) | Memory: 16.6 MB (70.98%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,48 +1,53 @@
 class Solution:
+    #   ==================
+    #   Depth-First Search
+    #   ==================
+
+    #   Performs DFS starting from each iterated
+    #   cell until each DFS branch is fully
+    #   searched and terminated (no possible
+    #   new branch)
+    
     def exist(self, board: list[list[str]], word: str) -> bool:
-        j_len = len(board)
-        i_len = len(board[0])
+        x = len(board[0])
+        y = len(board)
         
         word_len = len(word)
 
-        def search_next(j: int, i: int, idx: int):
-            #   Terminating case
+        def dfs(i: int, j: int, idx: int) -> bool:
+            #   Success
             if idx == word_len:
                 return True
-            
-            #   Boundary terminating cases
-            elif (i < 0) or (i >= i_len) or (j < 0) or (j >= j_len):
+            #   Terminating case: boundary
+            elif (i < 0) or (i >= x) or (j < 0) or (j >= y):
                 return False
             
-            #   Current iterated value
             temp = board[j][i]
 
-            #   Terminate current branch if doesn't extend word
+            #   Terminating case: wrong letter
             if temp != word[idx]:
                 return False
-            else:
-                #   Change to invalid value after found so
-                #   recursive calls doesn't branch to same cell
-                board[j][i] = '!'
-
-                #   Recursively branches to next cells
-                next = idx + 1
-                top = search_next(j - 1, i, next)
-                bottom = search_next(j + 1, i, next)
-                left = search_next(j, i - 1, next)
-                right = search_next(j, i + 1, next)
-
-                #   Restore value after recursive calls
-                board[j][i] = temp
-
-                #   Only returns 'True' if one of the branches
-                #   returns 'True'
-                return top or bottom or left or right
             
-        for i in range(i_len):
-            for j in range(j_len):
-                #   Terminate if recursive calls finds full word
-                if search_next(j, i, 0):
+            #   Mask checked cell in new
+            #   branch
+            board[j][i] = '!'
+
+            idx += 1
+            top = dfs(i, j - 1, idx)
+            bottom = dfs(i, j + 1, idx)
+            left = dfs(i - 1, j, idx)
+            right = dfs(i + 1, j, idx)
+
+            #   Unmask checked cell after
+            #   branching
+            board[j][i] = temp
+
+            #   Return branching result
+            return top or bottom or left or right
+            
+        for i in range(x):
+            for j in range(y):
+                if dfs(i, j, 0):
                     return True
 
         return False
